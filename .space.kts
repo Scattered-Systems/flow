9a9e9ac6ec4df3d32540d8864150ea786e4dd54d
FL03 <jo3mccain@icloud.com>
2022-07-16 02:46:00 -0500

    Update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- .space.kts
+++ .space.kts
@@ -1,14 +1,27 @@
 job("Flow: Build and Push Docker") {
+    startOn {
+        gitPush { enabled = false }
+    }
     docker {
+        // get auth data from secrets and put it to env vars
+        env["DOCKERHUB_USER"] = Secrets("dockerhub_user")
+        env["DOCKERHUB_TOKEN"] = Secrets("dockerhub_token")
+
+        // put auth data to Docker config
+        beforeBuildScript {
+            content = """
+                B64_AUTH=${'$'}(echo -n ${'$'}DOCKERHUB_USER:${'$'}DOCKERHUB_TOKEN | base64 -w 0)
+                echo "{\"auths\":{\"https://index.docker.io/v1/\":{\"auth\":\"${'$'}B64_AUTH\"}}}" > ${'$'}DOCKER_CONFIG/config.json
+            """
+        }
         build {
-            args["HTTP_PROXY"] = "http://0.0.0.0:8888"
             context = "."
             customPlatform = "linux/arm"
             file = "Dockerfile"
             labels["vendor"] = "scattered-systems"
         }
-        push("scattered-systems.registry.jetbrains.space/p/scsys/containers/flow") {
-            tags("0.0.\$JB_SPACE_EXECUTION_NUMBER")
+        push("jo3mccain/flow") {
+            tags("0.0.\$JB_SPACE_EXECUTION_NUMBER", "latest")
         }
     }
 }
