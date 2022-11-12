bd1401166f6e63ce732c87cc512d3636372ef181
Joe McCain III <jo3mccain@icloud.com>
2022-11-12 20:46:50 +0000

    Update

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-rustup install nightly
-rustup component add clippy rustfmt --toolchain nightly
-rustup target add wasm32-unknown-unknown --toolchain nightly
-sudo apt -y update && sudo apt -y upgrade && sudo apt -y autoremove && sudo apt install -y apt-utils protobuf-compiler
+sudo apt -y update && sudo apt -y upgrade && sudo apt -y autoremove
+sudo apt -y install apt-utils protobuf-compiler
