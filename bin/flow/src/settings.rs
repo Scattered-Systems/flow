9aebdcfc8af77bfeec5538eae632a917ea008771
Joe McCain III <jo3mccain@icloud.com>
2022-11-13 22:01:32 +0000

    Update

--- bin/flow/src/settings.rs
+++ bin/flow/src/settings.rs
@@ -64,7 +64,7 @@ impl Settings {
     pub fn build() -> ConfigResult<Self> {
         let mut builder = Config::builder()
             .add_source(
-                collect_config_files("**/.config/*", false)
+                collect_config_files("**/.config/*.toml", false)
             )
             .add_source(Environment::default().separator("__"));
         
