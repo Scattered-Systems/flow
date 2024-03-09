6d5b11e459e0b5106634f4200b8d25b1154b565a
Joe McCain III <jo3mccain@icloud.com>
2024-03-09 14:16:46 -0600

    update
    Signed-off-by: Joe McCain III <jo3mccain@icloud.com>

--- crates/fluidity/src/orch/mod.rs
+++ crates/fluidity/src/orch/mod.rs
@@ -13,24 +13,24 @@ pub mod chain;
 pub trait Orchestrate {
     type State;
 
-    fn execute(self, state: Self::State) -> Self::State;
+    fn execute(&self, state: Self::State) -> Self::State;
 }
 
-impl<T> Orchestrate for Vec<fn(State<T>) -> State<T>> where T: Clone {
+impl<T, E> Orchestrate for Vec<fn(State<T>) -> Result<State<T>, E>> where E: std::fmt::Debug, T: Clone {
     type State = State<T>;
 
-     fn execute(self, state: Self::State) -> Self::State {
+     fn execute(&self, state: Self::State) -> Self::State {
         self.iter().enumerate().fold(state, |output, (i, func)| {
             let new_state = output.clone();
             if new_state.stage.len() > i {
                 if new_state.stage[i] {
                     return new_state;
                 }
-                let mut next_state = func(new_state);
+                let mut next_state = func(new_state).unwrap();
                 next_state.stage[i] = next_state.proceed;
                 return next_state;
             }
-            let mut next_state = func(new_state);
+            let mut next_state = func(new_state).unwrap();
             next_state.stage.push(next_state.proceed);
             next_state
         })
